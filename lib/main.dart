d402b95bfd6e2a573321b899b3753518313fc263
Erickeyz <yunweneric@gmail.com>
2022-04-04 15:22:28 +0100

    updated main branch

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,8 @@
 import 'package:eva_barmananger/routes/AppRoutes.dart';
 import 'package:eva_barmananger/routes/onGeneratedRoute.dart';
 import 'package:eva_barmananger/utilities/constants.dart';
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:device_preview/device_preview.dart';
-
-// void main() => runApp(
-//       DevicePreview(
-//         enabled: !kReleaseMode,
-//         builder: (context) => MyApp(), // Wrap your app
-//       ),
-//     );
 
 void main() {
   runApp(MyApp());
@@ -35,8 +26,7 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
       ),
       initialRoute: AppRoutes.loginScreen,
-      onGenerateRoute: (settings) =>
-          Routegenerator.generatedRoutes(settings, context),
+      onGenerateRoute: (settings) => Routegenerator.generatedRoutes(settings, context),
     );
   }
 }
