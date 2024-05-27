d76b9cdcee641b84e0afe2887ccbc16f64fcf80f
yunweneric <yunweneric@gmail.com>
2024-05-27 10:35:21 +0100

    fetching laatst chaanges

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 
 void main() {
   runApp(const MyApp());
@@ -48,68 +49,65 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
+    // TODO: implement build
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+      body: Container(
+        color: Colors.orange.withOpacity(0.2),
+        height: MediaQuery.of(context).size.height,
+        width: MediaQuery.of(context).size.width,
+        // child: ListWheelScrollView(
+        //   itemExtent: 90,
+        //   diameterRatio: RenderListWheelViewport.defaultDiameterRatio,
+        //   // offAxisFraction: 0.7,
+        //   // useMagnifier: true,
+        //   // magnification: 0.1,
+        //   // squeeze: 0.8,
+        //   children: List.generate(20, (index) => index).map((e) {
+        //     int color = 0xFF222EEA + (e * 100);
+        //     return Container(
+        //       // height: 10,
+        //       width: double.infinity,
+        //       decoration: BoxDecoration(
+        //         color: Color(color),
+        //         borderRadius: BorderRadius.circular(20),
+        //       ),
+        //     );
+        //   }).toList(),
+        // ),
+
+        child: ListView.builder(
+          itemCount: 20,
+          itemBuilder: ((context, index) {
+            int color = 0xFF222EEA + (index * 100);
+            return Stack(
+              clipBehavior: Clip.none,
+              alignment: Alignment.center,
+              children: [
+                Container(
+                  width: double.infinity,
+                  height: 100,
+                ),
+                // Transform(
+                //   // duration: Duration(milliseconds: 100),
+                //   // top: -10.0 * index,
+                //   // left: 0,
+                //   // right: 0,
+                //   child: Container(
+                //     width: double.infinity,
+                //     height: 100,
+                //     decoration: BoxDecoration(
+                //       color: Color(color).withOpacity(0.7),
+                //       borderRadius: BorderRadius.circular(20),
+                //     ),
+                //   ),
+                // ),
+              ],
+            );
+          }),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
