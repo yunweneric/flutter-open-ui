2cea842fc083bd26c2f72ab980cf0ce7a5d56be3
yunweneric <yunweneric@gmail.com>
2024-06-10 12:34:15 +0100

    Added show case

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: "S'wips",
+      title: "S'wisp",
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         textTheme: TextTheme(
