60836623cccacfdb156ce3a488373e8105d8b3d8
yunweneric <yunweneric@gmail.com>
2024-06-13 04:47:24 +0100

    Added the chatbot_ui

---
+++ lib/utils/colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const bgColor = Color(0xFF010101);
+  static const yellow = Color(0xFFC6F432);
+  static const pink = Color(0xFFC09FF8);
+  static const purple = Color(0xFFFEC4DD);
+  static const white = Color(0xFFFFFFFF);
+}
