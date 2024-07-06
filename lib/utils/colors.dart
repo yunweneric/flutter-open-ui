5f31905c616d14db6f5933b9d3b3e2edfb9d285d
yunweneric <yunweneric@gmail.com>
2024-07-06 19:10:38 +0100

    Started implementation of the fruits

---
+++ lib/utils/colors.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const purpleLight = Color(0xffAE4EDB);
+  static const purple = Color(0xff6848C6);
+  static const greenLight = Color(0xffBCDB4E);
+  static const green = Color(0xff8EC648);
+  static const yellow = Color(0xffFFEE57);
+  static const yellowLight = Color(0xffFFB930);
+}
