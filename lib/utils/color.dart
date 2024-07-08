2c00884ee59b3ae73736223e984de5c14df1b99e
yunweneric <yunweneric@gmail.com>
2024-07-08 19:34:43 +0100

    Added assets and icons

---
+++ lib/utils/color.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const primary = Color(0xFFF3E3DA);
+  static const white = Color(0xFFFFFFFF);
+  static const black = Color(0xFF000000);
+  static const blue = Color(0xFF19C4EA);
+}
