d2673013fc105901c32ab04e84119e6016144860
yunweneric <yunweneric@gmail.com>
2024-07-08 06:55:21 +0100

    Reinitialised project

--- lib/utils/sizing.dart
+++
@@ -1,6 +0,0 @@
-import 'package:flutter/material.dart';
-
-class AppSizing {
-  static double width(context) => MediaQuery.of(context).size.width;
-  static double height(context) => MediaQuery.of(context).size.height;
-}
