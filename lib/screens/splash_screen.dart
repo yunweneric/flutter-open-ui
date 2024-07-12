b458524eb383662397e3d0c6a6eaa33141c4c229
yunweneric <yunweneric@gmail.com>
2024-07-12 22:04:17 +0100

    Initialised the splashscreen and utils

---
+++ lib/screens/splash_screen.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    Future.delayed(
+      const Duration(seconds: 1),
+      () => {},
+    );
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
