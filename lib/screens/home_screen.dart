b3be2e3d06cc647422e58ff87872ce6733b701f8
yunweneric <yunweneric@gmail.com>
2024-08-01 08:09:52 +0100

    Implemented the animation and fade in transitions

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,6 +1,9 @@
 import 'dart:math';
 
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:flutter_openui/routes/router.dart';
+import 'package:flutter_openui/screens/detail_screen.dart';
 import 'package:flutter_openui/utils/sizing.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -10,42 +13,84 @@ class HomeScreen extends StatefulWidget {
   State<HomeScreen> createState() => _HomeScreenState();
 }
 
-class _HomeScreenState extends State<HomeScreen> {
+class _HomeScreenState extends State<HomeScreen> with SingleTickerProviderStateMixin {
+  late AnimationController controller;
+  late Animation<double> fadeTextAnimation;
+
+  @override
+  void initState() {
+    controller = AnimationController(
+      vsync: this,
+      duration: Duration(milliseconds: 700),
+    );
+
+    fadeTextAnimation = Tween<double>(begin: 0, end: 1).animate(
+      CurvedAnimation(
+        parent: controller,
+        curve: Curves.easeOut,
+      ),
+    );
+
+    Future.delayed(Duration(milliseconds: 700), () => controller.forward());
+
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     List<int> items = List.generate(13, (item) => item);
     return Scaffold(
       body: Transform.rotate(
         angle: 0.5 * pi,
-        child: ListWheelScrollView(
-          perspective: 0.009,
-          itemExtent: AppSizing.height(context) * 0.15,
-          children: items.map(
-            (index) {
-              return GestureDetector(
-                onTap: () => {},
-                child: Transform.rotate(
-                  angle: -0.5 * pi,
-                  child: Container(
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(10),
-                      color: Colors.teal,
-                      image: DecorationImage(
-                        fit: BoxFit.cover,
-                        image: AssetImage(
-                          "assets/images/image_${index}.jpg",
-                        ),
-                      ),
-                    ),
-
-                    // height: AppSizing.height(context) * 0.1,
-                    width: AppSizing.width(context) * 0.3,
+        child: AnimatedBuilder(
+            animation: controller,
+            builder: (context, val) {
+              return Transform(
+                alignment: Alignment.center,
+                transform: Matrix4.identity()..scale(1.0),
+                child: Opacity(
+                  opacity: fadeTextAnimation.value,
+                  child: ListWheelScrollView(
+                    controller: ScrollController(initialScrollOffset: 200),
+                    perspective: 0.009,
+                    itemExtent: AppSizing.height(context) * 0.15,
+                    children: items.map(
+                      (index) {
+                        return GestureDetector(
+                          onTap: () async {
+                            await controller.reverse();
+                            final navigate = await AppRouter.navigate(context, DetailScreen(tag: index));
+                            if (navigate) {
+                              controller.forward();
+                            }
+                          },
+                          child: Transform.rotate(
+                            angle: -0.5 * pi,
+                            child: Hero(
+                              tag: index,
+                              child: ClipRRect(
+                                borderRadius: BorderRadius.circular(10),
+                                child: Image.asset(
+                                  "assets/images/image_${index}.jpg",
+                                  fit: BoxFit.cover,
+                                  width: AppSizing.width(context) * 0.3,
+                                ),
+                              ),
+                            ),
+                          ),
+                        );
+                      },
+                    ).toList(),
                   ),
                 ),
               );
-            },
-          ).toList(),
-        ),
+            }),
       ),
     );
   }
