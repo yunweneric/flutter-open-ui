3174b6c1bd1ceb9536cf6e53bb05be400ee0bdff
yunweneric <yunweneric@gmail.com>
2024-08-05 10:02:07 +0100

    Added ui for skew card

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -30,7 +30,7 @@ class _HomeScreenState extends State<HomeScreen> {
                   child: TweenAnimationBuilder(
                     curve: Curves.fastOutSlowIn,
                     key: ValueKey(isVisible),
-                    duration: const Duration(milliseconds: 1800),
+                    duration: const Duration(milliseconds: 1500),
                     tween: isVisible ? Tween<double>(begin: 1, end: 0) : Tween<double>(begin: 0, end: 1),
                     builder: (context, value, child) {
                       return Transform(
