ba5b96ed2aad9b9d14e5a697243105f8b9b7d075
yunweneric <yunweneric@gmail.com>
2024-09-04 08:15:44 +0100

    Added fade carousel

--- lib/main.dart
+++ lib/main.dart
@@ -5,20 +5,25 @@ void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
+      title: 'Fade Carousel',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: HomeScreen(),
+      home: const HomeScreen(),
     );
   }
 }
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,8 +1,8 @@
 import 'dart:math';
 
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 import 'package:flutter_openui/utils/sizing.dart';
+import 'package:flutter_svg/svg.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -11,28 +11,10 @@ class HomeScreen extends StatefulWidget {
   State<HomeScreen> createState() => _HomeScreenState();
 }
 
-class _HomeScreenState extends State<HomeScreen> with SingleTickerProviderStateMixin {
-  late AnimationController controller;
-  late Animation<double> fadeTextAnimation;
-
-  @override
-  void initState() {
-    controller = AnimationController(
-      vsync: this,
-      duration: Duration(milliseconds: 700),
-    );
-
-    fadeTextAnimation = Tween<double>(begin: 0, end: 1).animate(
-      CurvedAnimation(
-        parent: controller,
-        curve: Curves.easeOut,
-      ),
-    );
-
-    Future.delayed(Duration(milliseconds: 700), () => controller.forward());
-
-    super.initState();
-  }
+class _HomeScreenState extends State<HomeScreen> {
+  int activeIndex = 2;
+  final duration = const Duration(milliseconds: 700);
+  final controller = PageController(viewportFraction: 0.8, initialPage: 2);
 
   @override
   void dispose() {
@@ -42,47 +24,103 @@ class _HomeScreenState extends State<HomeScreen> with SingleTickerProviderStateM
 
   @override
   Widget build(BuildContext context) {
-    List<int> items = List.generate(13, (item) => item);
     return Scaffold(
-      body: Transform.rotate(
-        angle: 0.5 * pi,
-        child: AnimatedBuilder(
-            animation: controller,
-            builder: (context, val) {
-              return Transform(
-                alignment: Alignment.center,
-                transform: Matrix4.identity()..scale(1.0),
-                child: Opacity(
-                  opacity: fadeTextAnimation.value,
-                  child: ListWheelScrollView(
-                    controller: ScrollController(initialScrollOffset: 200),
-                    perspective: 0.009,
-                    itemExtent: AppSizing.height(context) * 0.15,
-                    children: items.map(
-                      (index) {
-                        return GestureDetector(
-                          onTap: () async {},
-                          child: Transform.rotate(
-                            angle: -0.5 * pi,
-                            child: Hero(
-                              tag: index,
-                              child: ClipRRect(
-                                borderRadius: BorderRadius.circular(10),
-                                child: Image.asset(
-                                  "assets/images/image_${index}.jpg",
-                                  fit: BoxFit.cover,
-                                  width: AppSizing.width(context) * 0.3,
-                                ),
-                              ),
-                            ),
-                          ),
-                        );
-                      },
-                    ).toList(),
-                  ),
+      backgroundColor: const Color.fromARGB(255, 236, 236, 236),
+      body: Stack(
+        children: [
+          PageView.builder(
+            controller: controller,
+            itemCount: 6,
+            onPageChanged: (page) {
+              controller.animateToPage(page, duration: Duration(milliseconds: 700), curve: Curves.easeOutExpo);
+              setState(() {
+                activeIndex = page;
+              });
+            },
+            itemBuilder: (c, i) {
+              return AppCard(index: i, activeIndex: activeIndex);
+            },
+          ),
+          Positioned(
+            top: AppSizing.height(context) * 0.7,
+            left: 0,
+            right: 0,
+            child: Transform.rotate(
+              angle: pi * -0.03,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [0, 1, 2, 3, 4, 5].map((item) {
+                  return AnimatedContainer(
+                    height: 12,
+                    width: 12,
+                    duration: duration,
+                    margin: const EdgeInsets.symmetric(horizontal: 3),
+                    decoration: BoxDecoration(
+                      color: activeIndex == item ? Colors.grey : Colors.grey.withOpacity(0.2),
+                      shape: BoxShape.circle,
+                    ),
+                  );
+                }).toList(),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class AppCard extends StatelessWidget {
+  final int index;
+  final int activeIndex;
+  const AppCard({super.key, required this.index, required this.activeIndex});
+  final duration = const Duration(milliseconds: 700);
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: AnimatedOpacity(
+        duration: duration,
+        opacity: activeIndex == index ? 1 : 0,
+        child: AnimatedContainer(
+          transformAlignment: Alignment.center,
+          transform: Matrix4.identity()
+            ..rotateZ(activeIndex == index
+                ? -0.1
+                : activeIndex > index
+                    ? -0.2
+                    : 0.2)
+            ..scale(activeIndex == index ? 1.0 : 0.5),
+          duration: const Duration(milliseconds: 400),
+          decoration: BoxDecoration(
+            border: Border.all(color: Colors.white, width: 8),
+            borderRadius: BorderRadius.circular(30),
+          ),
+          child: ClipRRect(
+            borderRadius: BorderRadius.circular(22),
+            child: Stack(
+              children: [
+                Image.asset(
+                  height: AppSizing.height(context) * 0.3,
+                  width: AppSizing.height(context) * 0.3,
+                  'assets/images/city_${index}.jpg',
+                  fit: BoxFit.cover,
                 ),
-              );
-            }),
+                Positioned(
+                  right: 20,
+                  top: 10,
+                  child: CircleAvatar(
+                    backgroundColor: Colors.white,
+                    child: Padding(
+                      padding: const EdgeInsets.all(10.0),
+                      child: SvgPicture.asset("assets/icons/favorite.svg"),
+                    ),
+                  ),
+                )
+              ],
+            ),
+          ),
+        ),
       ),
     );
   }
