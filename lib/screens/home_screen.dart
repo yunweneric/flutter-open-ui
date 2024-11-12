c65e4234556e09d3dded494ce08cc1b18f368b95
yunweneric <yunweneric@gmail.com>
2024-11-12 03:32:11 +0100

    adding animation to all items

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 
 class TileItem {
   final String title;
@@ -18,15 +19,15 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   int activeIndex = 0;
   List<TileItem> items = [
-    TileItem(title: "Primary", icon: '', color: Colors.blue),
-    TileItem(title: "Transactions", icon: '', color: Colors.green),
-    TileItem(title: "Updates", icon: '', color: Colors.deepPurple),
-    TileItem(title: "Promotions", icon: '', color: Colors.redAccent),
+    TileItem(title: "Primary", icon: 'assets/icons/user.svg', color: Colors.blue),
+    TileItem(title: "Transactions", icon: 'assets/icons/cart.svg', color: Colors.green),
+    TileItem(title: "Updates", icon: 'assets/icons/chat.svg', color: Colors.deepPurple),
+    TileItem(title: "Files", icon: 'assets/icons/files.svg', color: Colors.redAccent),
   ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Color(0xFFE6E6E6),
+      backgroundColor: const Color(0xFFE6E6E6),
       body: Padding(
         padding: const EdgeInsets.symmetric(horizontal: 20),
         child: Center(
@@ -48,7 +49,7 @@ class _HomeScreenState extends State<HomeScreen> {
                         maxLines: 1,
                         textDirection: TextDirection.ltr,
                       )..layout();
-                      final textWidth = textPainter.size.width + 50;
+                      final textWidth = textPainter.size.width + 40;
                       return InkWell(
                         onTap: () {
                           setState(() {
@@ -56,31 +57,34 @@ class _HomeScreenState extends State<HomeScreen> {
                           });
                         },
                         child: AnimatedContainer(
-                          duration: Duration(milliseconds: 500),
-                          margin: EdgeInsets.only(right: 10),
-                          padding: EdgeInsets.symmetric(vertical: 5, horizontal: 5),
+                          duration: const Duration(milliseconds: 500),
+                          margin: const EdgeInsets.only(right: 10),
+                          padding: const EdgeInsets.symmetric(vertical: 5, horizontal: 5),
                           decoration: BoxDecoration(
                             color: isActive ? item.color : Colors.grey,
                             borderRadius: BorderRadius.circular(10),
                           ),
                           width: isActive ? textWidth : 40,
-                          child: Row(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: [
-                              const Icon(Icons.card_travel),
-                              if (isActive) ...[
-                                SizedBox(width: 5),
-                                SizedBox(
-                                  // width: textWidth,
-                                  child: Text(
-                                    item.title,
-                                    softWrap: true,
-                                    maxLines: 1,
-                                    overflow: TextOverflow.clip,
+                          child: Padding(
+                            padding: const EdgeInsets.all(2.0),
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                SvgPicture.asset(item.icon, height: 15, color: isActive ? Colors.white : null),
+                                if (isActive) ...[
+                                  const SizedBox(width: 5),
+                                  SizedBox(
+                                    child: Text(
+                                      item.title,
+                                      softWrap: true,
+                                      maxLines: 1,
+                                      overflow: TextOverflow.clip,
+                                      style: TextStyle(fontSize: 12, color: isActive ? Colors.white : null),
+                                    ),
                                   ),
-                                ),
+                                ],
                               ],
-                            ],
+                            ),
                           ),
                         ),
                       );
