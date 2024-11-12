f5a84d4cef486a7f839ac6eee0d7e8bb0df22d07
yunweneric <yunweneric@gmail.com>
2024-11-12 02:38:50 +0100

    tab animatins

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,8 +1,21 @@
-import 'dart:math';
-
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
-import 'package:flutter_openui/utils/sizing.dart';
+
+class TileItem {
+  final String title;
+  final bool isCompleted;
+
+  TileItem({required this.title, required this.isCompleted});
+
+  TileItem copyWith({
+    String? title,
+    bool? isCompleted,
+  }) {
+    return TileItem(
+      title: title ?? this.title,
+      isCompleted: isCompleted ?? this.isCompleted,
+    );
+  }
+}
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -11,78 +24,88 @@ class HomeScreen extends StatefulWidget {
   State<HomeScreen> createState() => _HomeScreenState();
 }
 
-class _HomeScreenState extends State<HomeScreen> with SingleTickerProviderStateMixin {
-  late AnimationController controller;
-  late Animation<double> fadeTextAnimation;
-
-  @override
-  void initState() {
-    controller = AnimationController(
-      vsync: this,
-      duration: Duration(milliseconds: 700),
-    );
-
-    fadeTextAnimation = Tween<double>(begin: 0, end: 1).animate(
-      CurvedAnimation(
-        parent: controller,
-        curve: Curves.easeOut,
-      ),
-    );
-
-    Future.delayed(Duration(milliseconds: 700), () => controller.forward());
-
-    super.initState();
-  }
-
-  @override
-  void dispose() {
-    controller.dispose();
-    super.dispose();
-  }
-
+class _HomeScreenState extends State<HomeScreen> {
+  List<TileItem> items = [
+    TileItem(title: "Post on Twitter", isCompleted: false),
+    TileItem(title: "Do the dishes", isCompleted: true),
+    TileItem(title: "Prepare slides for presentation", isCompleted: false),
+    TileItem(title: "Enjoy the ride to uber island", isCompleted: true),
+    TileItem(title: "Pick up kids from school", isCompleted: false),
+  ];
   @override
   Widget build(BuildContext context) {
-    List<int> items = List.generate(13, (item) => item);
     return Scaffold(
-      body: Transform.rotate(
-        angle: 0.5 * pi,
-        child: AnimatedBuilder(
-            animation: controller,
-            builder: (context, val) {
-              return Transform(
-                alignment: Alignment.center,
-                transform: Matrix4.identity()..scale(1.0),
-                child: Opacity(
-                  opacity: fadeTextAnimation.value,
-                  child: ListWheelScrollView(
-                    controller: ScrollController(initialScrollOffset: 200),
-                    perspective: 0.009,
-                    itemExtent: AppSizing.height(context) * 0.15,
-                    children: items.map(
-                      (index) {
-                        return GestureDetector(
-                          onTap: () async {},
-                          child: Transform.rotate(
-                            angle: -0.5 * pi,
-                            child: Hero(
-                              tag: index,
-                              child: ClipRRect(
-                                borderRadius: BorderRadius.circular(10),
-                                child: Image.asset(
-                                  "assets/images/image_${index}.jpg",
-                                  fit: BoxFit.cover,
-                                  width: AppSizing.width(context) * 0.3,
-                                ),
+      backgroundColor: Color(0xFFE6E6E6),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 40),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ...items.map(
+              (item) {
+                return Container(
+                  margin: const EdgeInsets.only(bottom: 12),
+                  child: CheckboxListTile(
+                    // enableFeedback: false,
+                    // enabled: false,
+                    contentPadding: const EdgeInsets.symmetric(horizontal: 10),
+                    shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+                    tileColor: Colors.white,
+                    fillColor: WidgetStateColor.resolveWith((states) {
+                      if (states.contains(WidgetState.selected)) {
+                        return Colors.black;
+                      }
+                      return Colors.grey;
+                    }),
+                    checkboxShape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(4)),
+                    side: BorderSide.none,
+                    controlAffinity: ListTileControlAffinity.leading,
+                    value: item.isCompleted,
+                    onChanged: (value) {
+                      setState(() {
+                        final index = items.indexOf(item);
+                        final newItem = items[index].copyWith(isCompleted: value!);
+                        items[index] = newItem;
+                      });
+                    },
+                    title: LayoutBuilder(builder: (context, constraints) {
+                      // Measure text width
+                      final textPainter = TextPainter(
+                        text: TextSpan(
+                          text: item.title,
+                          style: const TextStyle(color: Colors.black),
+                        ),
+                        maxLines: 1,
+                        textDirection: TextDirection.ltr,
+                      )..layout();
+                      final textWidth = textPainter.size.width * 1.2;
+
+                      return Stack(
+                        children: [
+                          Text(item.title),
+                          AnimatedPositioned(
+                            duration: const Duration(milliseconds: 400),
+                            curve: Curves.decelerate,
+                            top: 2,
+                            bottom: 0,
+                            left: item.isCompleted ? 0 : -textWidth,
+                            child: Center(
+                              child: Container(
+                                height: 2,
+                                color: Colors.black,
+                                width: textWidth,
                               ),
                             ),
-                          ),
-                        );
-                      },
-                    ).toList(),
+                          )
+                        ],
+                      );
+                    }),
                   ),
-                ),
-              );
-            }),
+                );
+              },
+            ),
+          ],
+        ),
       ),
     );
   }
